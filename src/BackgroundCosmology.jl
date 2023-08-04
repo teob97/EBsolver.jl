ed73bf43e85ff77ece90c9299c401f566a69f949
Matteo Baratto <teo.baratto.97@gmail.com>
2023-08-04 18:13:39 +0200

    Added parameters structs

--- src/BackgroundCosmology.jl
+++ src/BackgroundCosmology.jl
@@ -12,7 +12,11 @@ struct DerivedParameters
 	Ω0_nu	:: Float64
 	Ω0_Λ	:: Float64
 	# Constuctor
-	#function DerivedParameters(x::CosmologicalParameters)
-	#	Ω0_γ = 2 * (π^2 / 30)
-	#end
+	function DerivedParameters(x::CosmologicalParameters)
+		H0 		= 100 * x.h
+		Ω0_γ 	= (16 * π^3 * G_SI * (k_b_SI * x.T0_CMB)^4) / (90 * ħ_SI^3 * c_SI^5 * H0^2)
+		Ω0_nu 	= x.N_eff * (7.0/8) * (4.0/10)^(4.0/3) * Ω0_γ
+		Ω0_Λ	= 1 - (x.Ω0_k + x.Ω0_B + x.Ω0_CDM + Ω0_γ + Ω0_nu)
+		new(Ω0_γ, Ω0_nu, Ω0_Λ)
+	end
 end
