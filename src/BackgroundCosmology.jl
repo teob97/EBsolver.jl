a164922d04cce3fcd3a41f05dc0887d67e3759c1
Matteo Baratto <teo.baratto.97@gmail.com>
2023-08-05 11:48:08 +0200

    Added tests for BackgroundCosmology

--- src/BackgroundCosmology.jl
+++ src/BackgroundCosmology.jl
@@ -1,6 +1,6 @@
-export BackgroundCosmology
+export BackgroundCosmology, eval_ρ0_crit
 
-eval_ρ0_crit(H0) = (3*H0^2)/(8*π*G_SI)
+eval_ρ0_crit(H0) = (3*(H0*(1000/3.086e22))^2)/(8*π*G_SI)
 eval_Ω0_γ(T0_CMB_kelvin::Float64, H0::Float64) = 2 * (π^2/30) * (k_b_SI*T0_CMB_kelvin)^4 / (ħ_SI^3 * c_SI^3 * eval_ρ0_crit(H0) * c_SI^2)
 eval_Ω0_nu(N_eff::Float64, T0_CMB_kelvin::Float64, H0::Float64) = N_eff * (7.0/8) * (4.0/10)^(4.0/3) * eval_Ω0_γ(T0_CMB_kelvin, H0)
 eval_Ω0_Λ(Ω0_k, Ω0_B, Ω0_CDM, Ω0_γ, Ω0_nu)	= 1 - (Ω0_k + Ω0_B + Ω0_CDM + Ω0_γ + Ω0_nu)
--- test/BackgroundCosmology.jl
+++ test/BackgroundCosmology.jl
@@ -1,2 +1,9 @@
 @test BackgroundCosmology(T0_CMB = 0.0).Ω0_γ  ≈ 0.0
 @test BackgroundCosmology(N_eff = 0.0).Ω0_nu  ≈ 0.0
+
+# The right sides have been taken from "Cosmology" [Weinberg 2ed]
+h_test = 0.67
+T_test = 2.7255
+@test eval_ρ0_crit(h_test*100) - 1.878e-26 * h_test^2 < 1e-3
+@test BackgroundCosmology(h = h_test, T0_CMB = T_test).Ω0_γ  - 2.47e-5 / h_test^2 < 1e-3
+
