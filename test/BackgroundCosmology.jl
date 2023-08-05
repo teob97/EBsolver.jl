df3e1684dd79f037055b18a12a5ebcc450400526
Matteo Baratto <teo.baratto.97@gmail.com>
2023-08-05 12:03:52 +0200

    Fixed some tests with relative tolerance

--- test/BackgroundCosmology.jl
+++ test/BackgroundCosmology.jl
@@ -4,6 +4,5 @@
 # The right sides have been taken from "Cosmology" [Weinberg 2ed]
 h_test = 0.67
 T_test = 2.7255
-@test eval_ρ0_crit(h_test*100) - 1.878e-26 * h_test^2 < 1e-3
-@test BackgroundCosmology(h = h_test, T0_CMB = T_test).Ω0_γ  - 2.47e-5 / h_test^2 < 1e-3
-
+@test isapprox(eval_ρ0_crit(h_test*100), 1.878e-26 * h_test^2; rtol=1e-4)
+@test isapprox(BackgroundCosmology(h = h_test, T0_CMB = T_test).Ω0_γ, 2.47e-5 / h_test^2; rtol=1e-2)
