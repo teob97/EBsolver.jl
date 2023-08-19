1b3cbb0964b4825ffcb861b78401695347ecdc4e
Matteo Baratto <teo.baratto.97@gmail.com>
2023-08-19 13:55:11 +0200

    Added tests for Hp''

--- test/BackgroundCosmology.jl
+++ test/BackgroundCosmology.jl
@@ -14,15 +14,20 @@ eta_derivative = BSplineKit.diff(bc.η_of_x)
 @test isapprox(mean(eta_derivative.(x).*bc.Hp_of_x(x)/c_SI), 1.0, rtol=1e-5)
 
 # Analitically, for a fluid with EoS P=wρ we have Hp'/Hp = d[log(Hp)]/dt = - [(1+3w) / 2] H(t)
-
 # Einstein–de Sitter universe (only matter with w=0) 
 bc = BackgroundCosmology(BackgroundCosmologySetup(Ω0_B=0.0, Ω0_CDM=1.0, Ω0_γ=0.0, Ω0_nu=0.0))
 @test (bc.dHpdx_of_x(x)./bc.Hp_of_x(x)) ≈ -0.5*bc.H_of_x(x)
-
 # Only radiation w=1/3
 bc = BackgroundCosmology(BackgroundCosmologySetup(Ω0_B=0.0, Ω0_CDM=0.0, Ω0_γ=1.0, Ω0_nu=0.0))
 @test (bc.dHpdx_of_x(x)./bc.Hp_of_x(x)) ≈ -bc.H_of_x(x)
-
 # Only lambda w=1/3
 bc = BackgroundCosmology(BackgroundCosmologySetup(Ω0_B=0.0, Ω0_CDM=0.0, Ω0_γ=0.0, Ω0_nu=0.0))
 @test (bc.dHpdx_of_x(x)./bc.Hp_of_x(x)) ≈ bc.H_of_x(x)
+
+# Analitically, for a fluid with EoS P=wρ we have Hp''/Hp = Hp'/Hp d[log(Hp')]/dt = [(2+9w+9w²) / 2] H(t)
+bc = BackgroundCosmology(BackgroundCosmologySetup(Ω0_B=0.0, Ω0_CDM=1.0, Ω0_γ=0.0, Ω0_nu=0.0))
+@test (bc.ddHpddx_of_x(x)./bc.Hp_of_x(x)) ≈ bc.H_of_x(x).^2
+bc = BackgroundCosmology(BackgroundCosmologySetup(Ω0_B=0.0, Ω0_CDM=0.0, Ω0_γ=1.0, Ω0_nu=0.0))
+@test (bc.ddHpddx_of_x(x)./bc.Hp_of_x(x)) ≈ 3*bc.H_of_x(x).^2
+bc = BackgroundCosmology(BackgroundCosmologySetup(Ω0_B=0.0, Ω0_CDM=0.0, Ω0_γ=0.0, Ω0_nu=0.0))
+@test (bc.ddHpddx_of_x(x)./bc.Hp_of_x(x)) ≈ bc.H_of_x(x).^2
