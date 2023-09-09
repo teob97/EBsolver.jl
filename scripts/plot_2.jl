0e6563da1815d7b9da3492b25a0c2d77e8c62ec1
Matteo Baratto <teo.baratto.97@gmail.com>
2023-09-09 11:33:04 +0200

    Added example script, not finished [skip CI]

---
+++ scripts/plot_2.jl
@@ -0,0 +1,122 @@
+using EBsolver
+using Plots
+using Logging
+
+let
+
+    @info "Setting up Background Cosmology"
+
+    path = "scripts/plots/RecombinationHistory/milestone2.png"
+    rh = RecombinationHistory()
+    bc = BackgroundCosmology()
+    x = range(rh.cosmo.x_start, -1e-2, 4000)
+
+    @info "Making τ(x) and derivatives graph"
+
+    p1 = plot(
+        x, 
+        τ_of_x(rh, false).(x),
+        yaxis=:log,
+        xlim=[rh.x_start, rh.x_end],
+        ylim=[10^-8, 10^8],
+        linewidth=2,
+        xlabel="x=log(a)",
+        label = "τ(x)"
+    )
+
+    p1 = plot!(
+        x, 
+        -dτdx_of_x(rh, false).(x),
+        linewidth=2,
+        xlabel="x=log(a)",
+        label = "-τ'(x)"
+    )
+
+    p1 = plot!(
+        x, 
+        ddτddx_of_x(rh, false).(x),
+        linewidth=2,
+        xlabel="x=log(a)",
+        label = "τ''(x)"
+    )
+
+    @info "Making τ(x) and derivatives graph with reionization"
+
+    p2 = plot(
+        x, 
+        τ_of_x(rh, true).(x),
+        yaxis=:log,
+        xlim=[rh.x_start, rh.x_end],
+        ylim=[10^-8, 10^8],
+        linewidth=2,
+        xlabel="x=log(a)",
+        label = "τ(x)"
+    )
+
+    p2 = plot!(
+        x, 
+        -dτdx_of_x(rh, true).(x),
+        linewidth=2,
+        xlabel="x=log(a)",
+        label = "-τ'(x)"
+    )
+
+    p2 = plot!(
+        x, 
+        ddτddx_of_x(rh, true).(x),
+        linewidth=2,
+        xlabel="x=log(a)",
+        label = "τ''(x)"
+    )
+
+    @info "Making X_e(x) graph"
+
+    p3 = plot(
+        x,
+        Xe_reion_of_x(rh).(x),
+        xlim=[rh.x_start, rh.x_end],
+        linewidth=2,
+        xlabel="x=log(a)",
+        label="X_e(x) with reionization",
+        line=:dash,
+    )
+
+    p3 = plot!(
+        x,
+        Xe_of_x(rh).(x),
+        linewidth=2,
+        label="X_e(x)"
+        )
+
+    @info "Making visibility function g(x) graph"
+    
+    p4 = plot(
+        x,
+        visibility_function_of_x(rh, false).(x),
+        xlim=[rh.x_start, rh.x_end],
+        linewidth=2,
+        xlabel="x=log(a)",
+        label="X_e(x) with reionization",
+        line=:dash,
+    )
+
+    p4 = plot!(
+        x,
+        visibility_function_of_x(rh, true).(x),
+        xlim=[rh.x_start, rh.x_end],
+        linewidth=2,
+        xlabel="x=log(a)",
+        label="X_e(x) with reionization",
+    )
+    
+    @info "Making g'(x) graph"
+    
+    @info "Making g''(x) graph"
+
+
+    plot(p1, p2, p3, p4, size = (1000, 600))
+    savefig(path)
+
+    @info "Image saved at "*path
+
+end
